64f7f8aec4bfacab70f3185816693b0f294d0377
Alyssa <tui91984@temple.edu>
2021-09-15 09:56:12 -0400

    Added behavior to the button (change text on click)

--- app/src/main/java/edu/temple/helloworld/MainActivity.kt
+++ app/src/main/java/edu/temple/helloworld/MainActivity.kt
@@ -2,10 +2,17 @@ package edu.temple.helloworld
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //add behavior to button
+        val button = findViewById<Button>(R.id.button)
+        val message = findViewById<TextView>(R.id.greetingText)
+        button.setOnClickListener { message.text = "You clicked the button" }
     }
 }
